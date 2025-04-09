12cda1f843763fd753f57bcf5a41480c21219693
yogesh-kumarasamy <yogesh.kumar@anywhere.co>
2025-04-09 17:07:34 +0530

    removed unused css file

--- quiz-ui/src/main.tsx
+++ quiz-ui/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css';
 import Quiz from './quiz';
 
 createRoot(document.getElementById('root')!).render(
